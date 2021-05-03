63816fcf199dcecc0394e9093a3d655961686612
Vivek Sharma <80456739+ifsvivek@users.noreply.github.com>
2021-05-03 09:51:19 +0530

    Update Cuberoot.cpp

--- Cuberoot.cpp
+++ Cuberoot.cpp
@@ -29,45 +29,10 @@ double cubicRoot(double n)
 int main()
 {
 
-    double n ;
-    clock_t time_req;
-    
+    double n; 
     printf("input number: ");
     scanf("%lf", &n);
     printf("Cubic root of %lf is %lf\n", n, cubicRoot(n));
     cout<<endl;
-        time_req = clock();
-	for(int i=0; i<100000; i++)
-	{
-		  double diff(double n,double mid)
-{
-    if (n > (mid*mid*mid))
-    return (n-(mid*mid*mid));
-    else
-    return ((mid*mid*mid) - n);
-}
-
-double cubicRoot(double n)
-{
-    double start = 0, end = n;
-    double e = 0.0000001;
-    while (true)
-
-    {
-        double mid = (start + end)/2;
-        double error = diff(n, mid);
-        if (error <= e)
-        return mid;
-        if ((mid*mid*mid) > n)
-        end = mid;
-        else
-        start = mid;
-
-    }
-};
-	}
-	time_req = clock() - time_req;
-	cout << "It took " << (float)time_req/CLOCKS_PER_SEC << " seconds" << endl;
-
     return 0;
 }
